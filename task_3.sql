877f86fe8f56d71d493a5a1d5ffa3c368c83c93a
Olajide Ojo <oolajide91@gmail.com>
2024-10-14 13:24:28 +0100

    show tables

---
+++ task_3.sql
@@ -0,0 +1,13 @@
+SET @dbName = 'alx_book_store';
+
+-- Prepare the statement to use the database
+SET @useDb = CONCAT('USE ', @dbName);
+
+-- Execute the prepared statement to switch databases
+PREPARE stmt FROM @useDb;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+-- list tables in alx_book_store databse
+USE @dbName;
+SHOW TABLES;
