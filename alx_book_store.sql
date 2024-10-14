fd74d303a525057db750c4dd52c9072ea7334796
Olajide Ojo <oolajide91@gmail.com>
2024-10-14 07:39:17 +0100

    remove unwanted spaces

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -46,9 +46,9 @@ CREATE TABLE Order_details (
 	order_id INT,
 	book_id INT,
 	quantity DOUBLE,
-	FOREIGN KEY (order_id )REFERENCES ORDERS(order _id )
+	FOREIGN KEY (order_id) REFERENCES ORDERS(order _id)
 	ON DELETE CASCADE,
-	FOREIGN KEY (book_ id )REFERENCES BOOKS(book _id )
+	FOREIGN KEY (book_ id) REFERENCES BOOKS(book _id)
 	ON DELETE CASCADE
 	
 );
