e5028e698aae1939950cf622d780fdbd1b851943
Olajide Ojo <oolajide91@gmail.com>
2024-10-14 14:14:45 +0100

    insert data into table

---
+++ task_5.sql
@@ -0,0 +1,16 @@
+SET TABLE_NAME = 'Customer';
+SET TABLE_SCHEMA = 'alx_book_store'
+
+SET @useDb = CONCAT('USE ', @tableSchema);
+PREPARE stmt FROM @useDb;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+-- Query to insert record in the specified table
+INSERT INTO Customers (
+   'customer_id', 
+   'customer_name', 
+   'email', 
+   'address'
+)
+VALUE (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')      
